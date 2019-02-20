e47a6009885817534ca5ca2fd7bb3c3847e921fc
Lesja01 <Lesja01@mail.ru>
2019-02-21 01:34:38 +0300

    add solutions

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let countTriangle=0;
+  let array=preferences;
+     
+   for (var i=0; i<array.length; i++){
+     	let a=array[i];        
+			let b=array[a-1];
+    	let c=array[b-1]; 
+              
+     if (c===i+1&&a!=i+1){
+        countTriangle++;            
+      };
+           
+    };//close for
+ 
+	return countTriangle/3;
+};//close getLoveTrianglesCount
